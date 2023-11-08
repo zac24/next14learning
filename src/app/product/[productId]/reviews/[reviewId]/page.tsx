e2a981f4408f41c7fee57ab872e71fb114740a4e
Prashant Dwivedi <dwi.pra24@gmail.com>
2023-11-09 02:26:49 +0530

    added page wise not-found

--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,8 @@
+import { notFound } from "next/navigation"
+
 export default function reviewDetails({params}:{params:{productId:String, reviewId:string}}){
+    if(parseInt(params.reviewId) > 1000){
+        notFound()
+    }
     return <h1>Welcome to review{params.reviewId} of product{params.productId}</h1>
 }
