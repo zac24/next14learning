6937c98b754c7abe71c81f6afcb78af5fd530ede
Prashant Dwivedi <dwi.pra24@gmail.com>
2023-11-13 01:51:00 +0530

    added Dynamic routes

---
+++ src/app/product/[productId]/page.tsx
@@ -0,0 +1,3 @@
+export default function prdouctDetail({params,} : {params:{productId: string};}) {
+    return <h1>Welcome to product {params.productId} detail page</h1>
+}
---
+++ src/app/product/page.tsx
@@ -0,0 +1,8 @@
+export default function product(){
+    return <>
+            <h1>This is the product listing page</h1>
+            <h2>- Product 1</h2>
+            <h2>- Product 2</h2>
+            <h2>- Product 3</h2>
+        </>
+}
