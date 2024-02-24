76ca853f4ed1d324e616ab0bbc91eb1e3e7c4517
Prashant Dwivedi <dwi.pra24@gmail.com>
2024-02-24 21:41:30 +0530

    title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "This is blog in next.js 14"
+    }
+}
+
 export default function blog() {
     return <h1>Welcome to Blog page!</h1>
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+    title: "Profile"
+}
+
 export default function profile(){
     return <h1>Welcome to profile page</h1>
 }
