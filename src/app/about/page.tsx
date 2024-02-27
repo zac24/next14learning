da89b05e00e67860d63d60390ef556e3d0c9f943
Prashant Dwivedi <dwi.pra24@gmail.com>
2024-02-27 22:01:42 +0530

    Static Metadata & Dynamic Metadata configuration

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title : "About Next.JS 14"
+}
+
 export default function about() {
     return <h1>Welcome to about page!</h1>
 }
--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,22 @@
-export default function prdouctDetail({params,} : {params:{productId: string};}) {
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId : string
+    }
+}
+
+export const generateMetadata = async ({ params}: Props): Promise<Metadata> => {
+    const title =  await new Promise( resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    })
+    return (
+        {title: `Product ${title}`}
+    )
+}
+
+export default function prdouctDetail({params,} : Props) {
     return <h1>Welcome to product {params.productId} detail page</h1>
 }
